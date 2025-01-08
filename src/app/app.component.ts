0f62e31f27f0c95bcb32adb4003963c1ef8b4797
tejaswi-28 <tejaswilatika28@gmail.com>
2025-01-08 17:53:43 +0530

    netlify.toml added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
+import { Component, Inject, inject, OnInit, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'project';
+  doc = inject(DOCUMENT);
+  
+  constructor(@Inject(PLATFORM_ID) private platformId:any){}
+
+  ngOnInit(): void {
+    if(isPlatformBrowser(this.platformId)){
+      console.log("Browser");
+    }
+  }
 }
