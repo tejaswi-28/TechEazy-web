7ea09f520a078c7bc56026e9aa7da06d11dd94d2
tejaswi-28 <tejaswilatika28@gmail.com>
2025-01-08 11:22:18 +0530

    Added angular.json

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-TOFDTDZD.mjs";import"./chunk-XLI2ZDJE.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-OANVSU7R.mjs";import"./chunk-XLI2ZDJE.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
